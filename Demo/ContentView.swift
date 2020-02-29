a38dc947bdc70032213beeac4801881116caea46
Gesen <i@gesen.me>
2020-02-29 15:31:34 +0800

    chore: add demo target

---
+++ Demo/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Demo
+//
+//  Created by Gesen on 2020/2/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

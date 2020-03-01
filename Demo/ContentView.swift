6dd1087e19dd84916b2f26ac6706bcc97811a7f0
Gesen <i@gesen.me>
2020-03-01 21:29:42 +0800

    feat: wip

--- Demo/ContentView.swift
+++ Demo/ContentView.swift
@@ -6,12 +6,46 @@
 //
 
 import SwiftUI
+import Refresh
 
 struct ContentView: View {
+    var count = 30
+    
     var body: some View {
-        Text("Hello, World!")
+        GeometryReader { proxy in
+            List {
+                Group {
+                    Refresh(.top) { (isRefreshing, progress) in
+                        Text("\(isRefreshing ? "true" : "false") \(progress)")
+                    }
+                    
+                    ForEach(0 ..< self.count) { i in
+                        Text("\(i)")
+                            .frame(maxHeight: 0)
+                    }
+                    
+                    Refresh(.bottom) { (isRefreshing, progress) in
+                        Text("\(isRefreshing ? "true" : "false") \(progress)")
+                    }
+                }
+                .preference(key: TestKey.self, value: [proxy.frame(in: .global)])
+            }
+        }
+        .offset(y: -44)
+        .onPreferenceChange(TestKey.self) { p in
+            print(p)
+        }
     }
 }
+struct TestKey: PreferenceKey {
+    
+    static var defaultValue: [CGRect] = []
+    
+    static func reduce(value: inout [CGRect], nextValue: () -> [CGRect]) {
+        value.append(contentsOf: nextValue())
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- Sources/Refresh/Refresh.swift
+++ Sources/Refresh/Refresh.swift
@@ -1,3 +1,59 @@
-struct Refresh {
-    var text = "Hello, World!"
+
+import SwiftUI
+
+@available(iOS 13.0, *)
+public struct Refresh<Content: View> {
+    
+    let content: (Parameter) -> Content
+
+    public init(_ position: Position, @ViewBuilder content: @escaping (Parameter) -> Content) {
+        self.content = content
+    }
+}
+
+@available(iOS 13.0, *)
+extension Refresh {
+    
+    public enum Position {
+        case top
+        case bottom
+    }
+    
+    public typealias Parameter = (isRefreshing: Bool, progress: CGFloat)
+}
+
+@available(iOS 13.0, *)
+extension Refresh: View {
+    
+    public var body: some View {
+        GeometryReader { proxy in
+            ZStack {
+                EmptyView()
+            }
+            .onAppear { print(proxy.frame(in: .global)) }
+        }
+        .listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 0))
+//        .frame(height: 0)
+//        .frame(maxHeight: 0)
+//        Section(header:
+//            GeometryReader { proxy in
+//                Color.clear.preference(key: TestKey.self, value: [proxy.frame(in: .global)])
+//            }
+//            .frame(height: 0)
+//            .onPreferenceChange(TestKey.self, perform: { frame in
+//                print(frame)
+//            })) {
+//                EmptyView()
+//        }
+//        .frame(height: 0)
+    }
+}
+
+struct TestKey: PreferenceKey {
+    
+    static var defaultValue: [CGRect] = []
+    
+    static func reduce(value: inout [CGRect], nextValue: () -> [CGRect]) {
+        value.append(contentsOf: nextValue())
+    }
 }
